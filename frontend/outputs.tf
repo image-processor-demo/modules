25182352a4c9f2f62b284e68febb87efd3ec9ddd
samimassoud <samimassoud67@gmail.com>
2026-01-04 22:25:43 +0200

    r

--- frontend/outputs.tf
+++ frontend/outputs.tf
@@ -2,3 +2,13 @@ output "cloudfront_domain_name" {
   description = "The domain name of the CloudFront distribution"
   value       = aws_cloudfront_distribution.frontend_distribution.domain_name
 }
+
+output "cloudfront_distribution_id" {
+  description = "The ID of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.frontend_distribution.id
+}
+
+output "frontend_bucket_name" {
+  description = "The name of the S3 bucket hosting the frontend assets"
+  value       = aws_s3_bucket.frontend_bucket.bucket
+}
