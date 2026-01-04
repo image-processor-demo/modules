312aee3abd278ba0c1a751d1cc40fe621d93f549
samimassoud <samimassoud67@gmail.com>
2026-01-04 16:54:03 +0200

    Hotfix: Changed CloudFront domain name output

--- frontend/outputs.tf
+++ frontend/outputs.tf
@@ -1,4 +1,4 @@
 output "cloudfront_domain_name" {
   description = "The domain name of the CloudFront distribution"
-  value       = aws_cloudfront_distribution.frontend.domain_name
+  value       = aws_cloudfront_distribution.frontend_distribution.domain_name
 }
